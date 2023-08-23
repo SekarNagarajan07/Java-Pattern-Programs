dd069de9c9f9b38d2f1a3ce16295cd8848fd2435
sekar N <sekarnagarajan1234@gmail.com>
2023-08-23 15:31:35 +0530

    p6

---
+++ Pattern6.java
@@ -0,0 +1,22 @@
+//pattern6:
+//
+//        *
+//      * *
+//    * * *
+//  * * * *
+//* * * * *
+public class Pattern6 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1;i<=n;i++){
+            for (int j = n;j>i;j--){
+                System.out.print(" ");
+            }
+            for (int k = 1;k<=i;k++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}

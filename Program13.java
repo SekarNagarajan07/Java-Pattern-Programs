31b3ed9f924404f13b3496ce35d44329586fa1d5
sekar N <sekarnagarajan1234@gmail.com>
2023-08-24 17:56:29 +0530

    p13

---
+++ Program13.java
@@ -0,0 +1,22 @@
+//pattern13:
+//
+//        A
+//       B B
+//      C C C
+//     D D D D
+//    E E E E E
+public class Program13 {
+    public static void main(String[] args) {
+        int alpha = 65;
+        int n = 5;
+        for (int i = 1;i<=n;i++){
+            for (int j = 1;j<=n - i;j++){
+                System.out.print(" ");
+            }
+            for (int j = 1;j<=i;j++){
+                System.out.print((char) (alpha + i - 1)+ " ");
+            }
+            System.out.println();
+        }
+    }
+}

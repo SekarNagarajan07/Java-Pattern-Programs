7b1436da0bc382a74575942c3edc14324903eb20
sekar N <sekarnagarajan1234@gmail.com>
2023-08-25 14:03:06 +0530

    p21

--- Pattern21.java
+++ Pattern21.java
@@ -1,2 +1,19 @@
-public class Pattern21 {
+//pattern21:
+//
+//        A
+//        B C
+//        D E F
+//        G H I J
+//        K L M N O
+class Pattern21 {
+    public static void main(String[] args) {
+        int k = 65;
+        for (int i= 1;i<=5;i++){
+            for (int j = 1;j<=i;j++){
+                System.out.print((char) (k) + " ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
 }

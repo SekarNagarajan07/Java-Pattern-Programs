5cb04add50b71a929ee690beed3aeebf3543bcaa
sekar N <sekarnagarajan1234@gmail.com>
2023-08-31 20:07:38 +0530

    p32

---
+++ Pattern32.java
@@ -0,0 +1,28 @@
+//pattern32:
+//
+//        * * * * *
+//        * * * *
+//        * * *
+//        * *
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        * * * * *
+public class Pattern32 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n;i>=1;i--){
+            for (int j = i;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = 2;i<=n;i++){
+            for (int j = i;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
